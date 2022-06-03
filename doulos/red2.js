1488f2e935f84cd076edf08015bf1cc770eb7e26
jacksmith206 <76547241+jacksmith206@users.noreply.github.com>
2022-06-03 07:25:16 +0300

    Update red2.js

--- doulos/red2.js
+++ doulos/red2.js
@@ -9,13 +9,7 @@ song.type = 'audio/mpeg';
 song.src = 'https://jacksmith206.github.io/doulos/New%20York%20Girls.mp3';
 
 
-<!--<p>
 
-var soundOne = new Audio('https://jacksmith206.github.io/doulos/New%20York%20Girls.mp3'),
-    soundTwo = new Audio('https://jacksmith206.github.io/doulos/Two Women.mp3'),
-  soundThree = new Audio('sound3.ogg');
-
-</p>-->
 
 
 
@@ -69,3 +63,10 @@ song.addEventListener('timeupdate',function() {
 	document.getElementById('seek').max = song.duration;
 	document.getElementById('seek').value = curtime;
 })
+<!--<p>
+
+var soundOne = new Audio('https://jacksmith206.github.io/doulos/New%20York%20Girls.mp3'),
+    soundTwo = new Audio('https://jacksmith206.github.io/doulos/Two Women.mp3'),
+  soundThree = new Audio('sound3.ogg');
+
+</p>-->
